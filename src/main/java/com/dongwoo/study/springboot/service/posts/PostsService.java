48c1634632323c729644d49d66549effe2697327
이동우 <ehddn216@gmail.com>
2021-06-21 19:36:26 +0900

    게시판 화면 구성하기(with 머스테치), Posts CRUD 구현

--- src/main/java/com/dongwoo/study/springboot/service/posts/PostsService.java
+++ src/main/java/com/dongwoo/study/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.dongwoo.study.springboot.service.posts;
 
 import com.dongwoo.study.springboot.domain.posts.Posts;
 import com.dongwoo.study.springboot.domain.posts.PostsRepository;
+import com.dongwoo.study.springboot.web.dto.PostsListResponseDto;
 import com.dongwoo.study.springboot.web.dto.PostsResponseDto;
 import com.dongwoo.study.springboot.web.dto.PostsSaveRequestDto;
 import com.dongwoo.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,5 +41,21 @@ public class PostsService {
         );
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // ==> .map( posts -> new PostsListResponseDto(posts) )
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                ()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id)
+        );
+
+        postsRepository.delete(posts);
     }
 }
